99e929d36569654c6aaa7fb4a0eda3151f4ebe76
liuliu <24372900@qq.com>
2025-02-08 00:22:40 +0800

    ci: update ci file

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 rm -f MouthKing.UI.Desktop/packages.lock.json
 dotnet publish -r linux-x64 -c Release -f net9.0 /p:RestoreLockedMode=true /p:Version=$1
 cd MouthKing.UI.Desktop/bin/Release/net9.0/linux-x64/publish 
-cp MouthKing.UI.Desktop MouthKing.UI.Desktop.bin 
+cp MouthKing MouthKing.bin 
